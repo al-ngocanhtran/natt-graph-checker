37e5c1296076557db51c91630fa97953cbba6220
Elly <elly@192.168.2.8>
2022-05-27 20:59:35 +0700

    Complete

--- graph.py
+++ graph.py
@@ -33,10 +33,11 @@ class Graph:
         # Recur to check all the adjacent vertices to the current vertice
         print('self Graph: ', self.graph)
         neighbors = self.graph[v]
-        for i in neighbors:
-            if visited[i] == False:
-                print('check visited v', i)
-                self.DFSearch(i, visited)
+        print('neighbors: ', neighbors)
+        for key in neighbors:
+            if visited[key] == False:
+                print('check visited v', key)
+                self.DFSearch(key, visited)
 
      # Creating a graph with the given vertices
     def createEdge(self):
@@ -47,20 +48,23 @@ class Graph:
                 if v1 != v2:
                     self.graph[v1].append(v2)
                     self.graph[v2].append(v1)
-                    
+
     # Check if graph is connected
     def isConnected(self):
         # initializing a list of vertices with none visited
         size = len(self.graph)
-        visited = [False]*(size)
+        visited = {}
+        for key in self.listV:
+            visited[key] = False
         print('Visited: ', visited)
 
         # DFS should start visitedV
-        visitedV = 0
+        visitedV = ''
+        indexV = 0
         # Find a vertex with non-zero degree to start the algorithm
         for index, key in enumerate(self.graph):
             if len(self.graph[key]) > 1:
-                visitedV = index
+                visitedV = key
                 break
             # If there are no edges in the graph, return true
             if index == size - 1:
@@ -68,9 +72,11 @@ class Graph:
 
         self.DFSearch(visitedV, visited)
 
+        print('last visited: ', visited)
         # Check if all non-zero degree vertices are visited
-        for j in range(size):
-            if visited[j] == False and len(self.graph[j]) > 0:
+        for j, key in enumerate(self.graph):
+            if visited[key] == False and len(self.graph[key]) > 0:
+                print('FALINg')
                 return False
         return True
 
@@ -79,9 +85,12 @@ class Graph:
         if self.isConnected() == False:
             return 0
         else:
+            print('come here')
             # Meet condition: Graph is connected
             odd = 0
-            for i in range(len(self.graph)):
+            for i in self.graph:
+                print('Length of ver: ', len(self.graph[i]))
+                print()
                 if len(self.graph[i]) % 2 != 0:
                     odd += 1
             '''
@@ -90,6 +99,7 @@ class Graph:
             - odd = 0 => Graph is eulerian 
             - odd > 2 => Graph is not eulerian 
             '''
+            print(odd)
             if odd == 0:
                 return 2
             elif odd == 2:
@@ -99,14 +109,33 @@ class Graph:
 
     def test(self):
         res = self.isEulerian()
-        if res == 0:
+        if res == 1:
             # Check Planar here
-            return "Graph is not Eulerian graph"
-        elif res == 1:
-            return "Graph is a Semi-eulerian graph (having a Euler path)"
-        else:
+            return "Graph is a Semi-Eulerian graph (having a Euler path)"
+        elif res == 2:
             return "Graph is a Eulerian graph (having a Euler cycle)"
+        else:
+            return "Graph is a not Eulerian graph"
 
+    # Planarity
+    def findCyclce(self):
+        pass
+
+
+# class TestPlanarity:
+#     def __init__(self, graph):
+#         self.graph = graph
+#         self.result = None
+
+#     def isPlanar(self):
+#         Graph cycle = self.graph.findCycle()
+#         if len(self.listE) > 3 * len(self.listV) - 6:
+#             return 3
+#         # Split into multiple connected components
+#         pieces = self.graph.splitIntoPieces()
+
+#     def testPlanarity(self):
+#         return self.result
 
 class Visualization:
 
@@ -126,9 +155,8 @@ class Visualization:
         ax.set_title(result)
         nx.draw(G, with_labels=True, node_color="pink")
         plt.show(block=False)
-        plt.pause(5)
         # plt.savefig(filename)
-        plt.close()
+        # plt.close()
 
     def main(listE, listV):
         G = nx.Graph()
